fc549c508f75985a2f009cc34a2a92cd49e93342
BruceVim <qrfvim@163.com>
2019-10-14 01:18:13 +0800

    V4

--- smt.php
+++ smt.php
@@ -18,7 +18,7 @@ class CrawlerApp {
     public $num = 0;
     public $reNum = 0;
     public $UpNum = 50;
-    public $targetUrl = 'http://kaijiang.500.com/shtml/dlt/19052.shtml';
+
 
     private $MysqlHost = 'localhost';
     private $MysqlUser = 'root';
@@ -34,9 +34,9 @@ class CrawlerApp {
 
     public $MysqlConn;
 
-    public function Crawler( $driver, $page_id ) {
+    public function Crawler( $driver ) {
 
-            $targetUrl = 'https://www.aliexpress.com/item/32443138475.html';
+            $targetUrl = 'https://www.aliexpress.com/item/4000156718164.html';
             $driver->get( $targetUrl );
 
             #获取多个元素
@@ -54,35 +54,197 @@ class CrawlerApp {
             $_title = $driver -> findElements(
                 WebDriverBy::className('product-title')
             );
-            $smt_info['product_title'] = $_title[0] -> getText();
+            $_title_str = $_title[0] -> getText();
+            if( strlen($_title_str) > 0 )
+            {
+                $smt_info['product_title'] = $_title[0] -> getText();
+            }
             unset( $_title );
 
-            $_description = $driver -> findElements(
-                WebDriverBy::className('product-description')
+            #获取描述
+            $localtion = WebDriverBy::className('product-description');
+            if ( $this ->findElementExsit($driver, $localtion) ) {
+                $_description = $driver -> findElements(
+                    $localtion
+                );
+                $_description_str = $_description[0] -> getAttribute('innerHTML');
+                if( strlen($_description_str) > 0 )
+                {
+                    $smt_info['product_description'] = addslashes($_description[0] -> getAttribute('innerHTML'));
+                }
+
+            }
+            unset($localtion);
+            unset($_description);
+
+            #获取价格区间
+            $_price_range = $driver -> findElements(
+                WebDriverBy::className('product-price-value')
+            );
+            $smt_info['product_msrp'] = $_price_range[0] -> getAttribute('innerHTML');
+            $price_range_arr = explode(' ', $smt_info['product_msrp']);
+            $smt_info['product_msrp_min'] = str_replace('$', '', $price_range_arr[1]);
+            $smt_info['product_msrp_max'] = $price_range_arr[3];
+
+            $smt_info['product_cost_price'] = $_price_range[1] -> getAttribute('innerHTML');
+            $price_range_arr = explode(' ', $smt_info['product_cost_price']);
+            $smt_info['product_cost_min'] = str_replace('$', '', $price_range_arr[1]);
+            $smt_info['product_cost_max'] = $price_range_arr[3];
+            unset( $_price_range );
+
+            #获取库存
+            $_product_stock = $driver -> findElements(
+                WebDriverBy::className('product-quantity-tip')
+            );
+            $_product_stock_str = $_product_stock[0] -> getAttribute('innerHTML');
+            if ( strlen($_product_stock_str) > 0 )
+            {
+                preg_match_all('/\d+/',$_product_stock_str,$arr);
+                $smt_info['product_stock'] = $arr[0];
+            }
+            unset( $_product_stock );
+            unset( $_product_stock_str );
+
+            #到货时间
+            $_product_shipping_date = $driver -> findElements(
+                WebDriverBy::xpath('//span[@class=\'product-shipping-date\']/span[@class="product-shipping-delivery"]/span')
+            );
+            $_product_shipping_date_str = $_product_shipping_date[0] -> getAttribute('innerHTML');
+            $smt_info['product_stock'] = strlen($_product_shipping_date_str) > 0 ? $_product_shipping_date_str : '';
+            unset( $_product_shipping_date_str );
+            unset( $_product_shipping_date );
+
+            #属性获取
+            $_product_attr = $driver -> findElements(
+                WebDriverBy::xpath('//div[@class=\'product-sku\']/div[@class="sku-wrap"]/div[@class="sku-property"]')
             );
 
-            if ( $this ->findElementExsit($_description) ) {
-                var_dump($_description);
-                var_dump($_description[0]);
-                var_dump($_description[0]  -> getText() );
+            if ( !empty($_product_attr) )
+            {
+                foreach ( $_product_attr as $key =>  $attr )
+                {
+                    $_attr_name = $attr -> findElements(
+                        WebDriverBy::xpath('./div[@class=\'sku-title\']')
+                    );
+                    $product_attr_1_name = str_replace(': ', '' , $_attr_name[0] -> getAttribute('innerHTML'));
+                    $product_attr_1_name = $this -> remove_html_tag($product_attr_1_name);
+
+                    $_attr_img = $attr -> findElements(
+                        WebDriverBy::xpath('./ul[@class=\'sku-property-list\']//img')
+                    );
+                    $_attr_span = $attr -> findElements(
+                        WebDriverBy::xpath('./ul[@class=\'sku-property-list\']//div[@class="sku-property-text"]//span')
+                    );
+
+                    if ( !empty($_attr_img) )
+                    {
+                        foreach ( $_attr_img as $_img)
+                        {
+                            $img_str = $_img -> getAttribute('src');
+                            $img_title = $_img -> getAttribute('title');
+                            $img_index = strpos($img_str,'jpg');
+                            $img_str = substr($img_str, 0, $img_index +3 );
+
+                            $smt_info['product_attr'][ $product_attr_1_name ][ $img_title ] = $img_str;
+                        }
+                    }
+                    if ( !empty($_attr_span) )
+                    {
+                        foreach ( $_attr_span as $_span)
+                        {
+                            $span_str = $_span -> getAttribute('innerHTML');
+
+                            $smt_info['product_attr'][ $product_attr_1_name ][] = $span_str;
+                        }
+                    }
+
+                    unset($product_attr_1_name);
+                }
             }
 
-            
+            #获取属性价格
+            if ( !empty($smt_info['product_attr']) )
+            {
+                $dept = count($smt_info['product_attr']);
 
+                for ( $i = 0; $i < $dept ; $i ++ )
+                {
+
+
+                }
+                var_dump( $dept );
+            }
+
+
+
+            return;
     }
 
-    public function findElementExsit( $obj )
+    public function findElementExsit( $driver, $obj )
     {
-        if( $this -> isElementExsit($driver, $obj)){
+        if( $this -> isElementExsit($driver, $obj) ){
             return true;
         } else {
+
             $js="var q=document.documentElement.scrollTop=".(1000);
             $sScriptResult = $driver -> executeScript( $js, array() );
 
-            $this -> findElementExsit($obj);
+            return $this -> findElementExsit($driver, $obj);
         }
     }
 
+    /**
+     * 判断元素是否存在
+     * @param WebDriver $driver
+     * @param WebDriverBy $locator
+     */
+    function isElementExsit($driver,$locator){
+        try {
+            $nextbtn = $driver->findElement($locator);
+            return true;
+        } catch (\Exception $e) {
+            //echo 'element is not found!';
+            return false;
+        }
+    }
+
+
+    /**
+     * #去掉字符串html代码
+     * @param $html_string
+     * @param bool $is_save_br
+     * @return string
+     */
+    function remove_html_tag( $html_string, $is_save_br = false )
+    {
+        $description_content = strip_tags( $html_string );
+        if( '>' == substr( $description_content, 0, 1 ) )
+        {
+            $description_content = substr( $description_content, 1 );
+        }
+        $description_content = preg_replace('|style=\"[\w\W]+\">|', '', $description_content);
+        $description_content = str_replace('&nbsp;', '', $description_content);
+        $description_content = str_replace('&quot;', '"', $description_content);
+        $description_content = str_replace("&apos;","'",$description_content);
+        $description_content = str_replace("&amp;","&",$description_content);
+        $description_content = str_replace("&#39;","'",$description_content);
+        $description_content = preg_replace('/<table[^>]*?>.*?<\/table>/is'," ",$description_content);
+        //$description_content = str_replace(PHP_EOL, "\r\n", $description_content);
+        $description_content = nl2br( $description_content );
+
+        if ( !$is_save_br )
+        {
+            $description_content = preg_replace('/\s*(<br\s*\/?\s*>\s*){2,}/im',PHP_EOL,$description_content);
+            /*if( '<br />' == substr( $description_content,0, 6 ) )
+            {
+                $description_content = substr( $description_content, 6 );
+            }*/
+            $description_content = str_replace("<br />",PHP_EOL,$description_content);
+        }
+
+
+        return trim( $description_content );
+    }
 
     public function RunApp() {
 
